2c1659d021986d62c7b2468d23c3b767910b2498
Mashkin E.V. <mev.86@list.ru>
2025-04-06 21:50:03 +0300

    backend is ready

--- backend/api_app/views.py
+++ backend/api_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, comment):
+        post_id = self.request.data.get('post')
+        try:
+            post = Post.objects.get(id=post_id)
+            comment.save(post=post)
+        except Post.DoesNotExist:
+            raise ValidationError("Поста с этим идентификатором не существует")
